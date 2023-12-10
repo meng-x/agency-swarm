3fd34eb025de7a93b70f9abc3d14ce3772669018
joe <t@tm>
2023-12-10 22:18:46 +0900

    make it work.

--- tests/demo_gradio.py
+++ tests/demo_gradio.py
@@ -1,15 +1,18 @@
 import sys
+import os
 
 import gradio as gr
+from dotenv import load_dotenv
+load_dotenv()
+sys.path.insert(0, '..')
 
 from agency_swarm import set_openai_key
-
-sys.path.insert(0, '../agency-swarm')
-
 from agency_swarm.agency.agency import Agency
 from tests.ceo.ceo import Ceo
 from tests.test_agent.test_agent import TestAgent
 from tests.test_agent2.test_agent2 import TestAgent2
+
+set_openai_key(os.getenv('OPENAI_API_KEY'))
 
 test_agent1 = TestAgent()
 test_agent2 = TestAgent2()
@@ -19,8 +22,8 @@ agency = Agency([
     ceo,
     [ceo, test_agent1, test_agent2],
     [ceo, test_agent2],
-], shared_instructions="./manifesto.md")
+], shared_instructions="Talk in Japanese with each other.")
 
 
-agency.demo_gradio(height=1500)
+agency.demo_gradio(height=700)
 
--- tests/test_agent2/test_agent2.py
+++ tests/test_agent2/test_agent2.py
@@ -1,8 +1,7 @@
 from agency_swarm.agents import Agent
 
-
 class TestAgent2(Agent):
     def __init__(self):
         super().__init__()
         self.description = "Test Agent"
-        self.instructions = "./instructions.md"
+        self.instructions = "You are a test agent. Do what you are told."
